a9863066cb34884c802ce9eedd6e081d500bc475
iván martín <ivan.martin.78@gmail.com>
2020-04-21 14:41:10 +0200

    Code First OK

--- ZMLabsData/ZMLabsData/context/LabsContext.cs
+++ ZMLabsData/ZMLabsData/context/LabsContext.cs
@@ -17,10 +17,7 @@ namespace ZMLabsData.context
 
     public class LabsContext : DbContext
     {
-        public LabsContext(string cnx_str) : base(cnx_str)
-        {
-            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<context.LabsContext, Migrations.Configuration>());
-        }
+        public LabsContext(string cnx_str) : base(cnx_str) {  }
 
         public DbSet<EFModels.Categories> Categories { get; set; }
         public DbSet<EFModels.Tests> Test { get; set; }
